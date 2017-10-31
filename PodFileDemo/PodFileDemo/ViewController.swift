3ca8a4b14808be23c16cc4e8701931fc2843cd10
samcom-arun <samcom@MAC3-3.local>
2017-10-31 15:50:53 +0530

    init

---
+++ PodFileDemo/PodFileDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PodFileDemo
+//
+//  Created by MAC3 on 10/31/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Niki");
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
